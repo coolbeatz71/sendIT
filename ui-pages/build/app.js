3d5ea4f8b114c7bb062850497344e7fef39a4a9a
coolbeatz71 <sigmacool@gmail.com>
2018-11-02 21:17:33 +0200

    Editing architecture of the project by:     - adding 'createParcel.html' page,     - adding 'editParcel.html' page,     - adding 'userProfile.html' page, [finishes #161689447]

--- ui-pages/build/app.js
+++ ui-pages/build/app.js
@@ -7,13 +7,30 @@ var gotoSignUp = document.querySelector("#goto-sign-up");
 var gotoSignIn = document.querySelector("#goto-sign-in");
 var signUpForm = document.querySelector("#sign-up-form");
 var signInForm = document.querySelector("#sign-in-form");
+var signInLink = document.getElementById("sign-in-link");
+
+console.log(signInLink);
 
 btnStart.addEventListener('click', function () {
+    //open the modal
     modal.style.display = 'block';
+
+    //and the signUp form
+    signInForm.style.display = 'none';
+    signUpForm.style.display = 'block';
 });
 
 btnClose.addEventListener('click', function () {
     modal.style.display = 'none';
+});
+
+signInLink.addEventListener('click', function () {
+    //open the modal
+    modal.style.display = 'block';
+
+    //and the signIn form
+    signUpForm.style.display = 'none';
+    signInForm.style.display = 'block';
 });
 
 gotoSignIn.addEventListener('click', function () {
--- ui-pages/js/app.js
+++ ui-pages/js/app.js
@@ -5,13 +5,31 @@ let gotoSignUp = document.querySelector("#goto-sign-up");
 let gotoSignIn = document.querySelector("#goto-sign-in");
 let signUpForm = document.querySelector("#sign-up-form");
 let signInForm = document.querySelector("#sign-in-form");
+let signInLink = document.getElementById("sign-in-link");
+
+console.log(signInLink);
 
 btnStart.addEventListener('click', () => {
+    //open the modal
     modal.style.display = 'block';
+
+    //and the signUp form
+    signInForm.style.display = 'none';
+    signUpForm.style.display = 'block';
 });
 
 btnClose.addEventListener('click', () => {
     modal.style.display = 'none';
+});
+
+signInLink.addEventListener('click', () => {
+    //open the modal
+    modal.style.display = 'block';
+
+    //and the signIn form
+    signUpForm.style.display = 'none';
+    signInForm.style.display = 'block';
+
 });
 
 gotoSignIn.addEventListener('click', () => {
