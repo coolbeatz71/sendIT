2f976ac16b8d20b69ac3e29ffc37c75909a3b443
coolbeatz71 <sigmacool@gmail.com>
2018-11-02 19:44:19 +0200

    Adding Eventlistener to each links going either to sign-up or sign-in form     - Already a member?     - Not yet a member? Adding the style for each form, helping to switch between them
    [finishes #161686960]

--- ui-pages/build/app.js
+++ ui-pages/build/app.js
@@ -3,6 +3,10 @@
 var modal = document.getElementById("modal-sign-up");
 var btnStart = document.querySelector(".btn-start");
 var btnClose = document.querySelector(".btn-close-modal");
+var gotoSignUp = document.querySelector("#goto-sign-up");
+var gotoSignIn = document.querySelector("#goto-sign-in");
+var signUpForm = document.querySelector("#sign-up-form");
+var signInForm = document.querySelector("#sign-in-form");
 
 btnStart.addEventListener('click', function () {
     modal.style.display = 'block';
@@ -10,6 +14,16 @@ btnStart.addEventListener('click', function () {
 
 btnClose.addEventListener('click', function () {
     modal.style.display = 'none';
+});
+
+gotoSignIn.addEventListener('click', function () {
+    signUpForm.style.display = 'none';
+    signInForm.style.display = 'block';
+});
+
+gotoSignUp.addEventListener('click', function () {
+    signInForm.style.display = 'none';
+    signUpForm.style.display = 'block';
 });
 
 window.addEventListener('click', function (e) {
--- ui-pages/js/app.js
+++ ui-pages/js/app.js
@@ -1,6 +1,10 @@
-let modal    = document.getElementById("modal-sign-up");
-let btnStart = document.querySelector(".btn-start");
-let btnClose = document.querySelector(".btn-close-modal");
+let modal      = document.getElementById("modal-sign-up");
+let btnStart   = document.querySelector(".btn-start");
+let btnClose   = document.querySelector(".btn-close-modal");
+let gotoSignUp = document.querySelector("#goto-sign-up");
+let gotoSignIn = document.querySelector("#goto-sign-in");
+let signUpForm = document.querySelector("#sign-up-form");
+let signInForm = document.querySelector("#sign-in-form");
 
 btnStart.addEventListener('click', () => {
     modal.style.display = 'block';
@@ -8,6 +12,16 @@ btnStart.addEventListener('click', () => {
 
 btnClose.addEventListener('click', () => {
     modal.style.display = 'none';
+});
+
+gotoSignIn.addEventListener('click', () => {
+    signUpForm.style.display = 'none';
+    signInForm.style.display = 'block';
+});
+
+gotoSignUp.addEventListener('click', () => {
+    signInForm.style.display = 'none';
+    signUpForm.style.display = 'block';
 });
 
 window.addEventListener('click', (e) => {
